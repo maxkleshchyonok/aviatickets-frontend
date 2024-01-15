34252554ee6242d38e39ee52abfdb5e2b28303a4
tantality <angel.tretyak@mail.ru>
2024-01-15 14:40:04 +0300

    feat: change types of actions

--- src/app/ticket-search-filter/store/ticket-search-filter.actions.ts
+++ src/app/ticket-search-filter/store/ticket-search-filter.actions.ts
@@ -1,21 +1,37 @@
 import { createAction } from "@reduxjs/toolkit";
+import { TicketSearchFilterState } from "../types/ticket-search-filter.store";
 
-export const specifyOriginCity = createAction("filter/specify-origin-city", (originCity: string) => {
-  return { payload: originCity };
-});
+export const specifyOriginCity = createAction(
+  "filter/specify-origin-city",
+  (originCity: Pick<TicketSearchFilterState, "originCity">["originCity"]) => {
+    return { payload: originCity };
+  }
+);
 
-export const specifyDestinationCity = createAction("filter/specify-destination-city", (destinationCity: string) => {
-  return { payload: destinationCity };
-});
+export const specifyDestinationCity = createAction(
+  "filter/specify-destination-city",
+  (destinationCity: Pick<TicketSearchFilterState, "destinationCity">["destinationCity"]) => {
+    return { payload: destinationCity };
+  }
+);
 
-export const specifyDepartureTime = createAction("filter/add-departure-time", (departureTime: Date) => {
-  return { payload: departureTime };
-});
+export const specifyDepartureTime = createAction(
+  "filter/add-departure-time",
+  (departureTime: Pick<TicketSearchFilterState, "departureTime">["departureTime"]) => {
+    return { payload: departureTime };
+  }
+);
 
-export const specifyArrivalTime = createAction("filter/specify-arrival-time", (arrivalTime: Date) => {
-  return { payload: arrivalTime };
-});
+export const specifyArrivalTime = createAction(
+  "filter/specify-arrival-time",
+  (arrivalTime: Pick<TicketSearchFilterState, "arrivalTime">["arrivalTime"]) => {
+    return { payload: arrivalTime };
+  }
+);
 
-export const specifyPassengerAmount = createAction("filter/specify-passenger-amount", (passengerAmount: number) => {
-  return { payload: passengerAmount };
-});
+export const specifyPassengerAmount = createAction(
+  "filter/specify-passenger-amount",
+  (passengerAmount: Pick<TicketSearchFilterState, "passengerAmount">["passengerAmount"]) => {
+    return { payload: passengerAmount };
+  }
+);
