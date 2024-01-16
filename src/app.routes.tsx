63f9a98338a0420bfa16a5787fd6614332e37a0c
tantality <angel.tretyak@mail.ru>
2024-01-16 14:23:08 +0300

    feat: adjust routing for the module

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -5,8 +5,6 @@ import AuthRoutes from "./app/auth/auth.routes";
 import { Account } from "./app/account/Account";
 import { BookingPage } from "./app/booking";
 
-
-// ======= private route ======= //
 const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
     return sessionStorage.getItem('access_token') ? (
         <Suspense fallback={<div />}>
@@ -17,16 +15,13 @@ const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
     );
 };
 
-// ======= public route ======= //
 const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
     <Suspense fallback={<div />}>
         <Element />
     </Suspense>
 );
 
-// ======= pages ======= //
-// const MainPage = React.lazy(() => import("./app/main/Main"));
-// const BookingPage = React.lazy(() => import('./app/booking'));
+const TicketsRoutes = React.lazy(() => import('app/tickets'));
 
 export const AppRoutes = () => {
     return (
@@ -39,10 +34,10 @@ export const AppRoutes = () => {
 
             {/* PRIVATE */}
             <Route path={"/account"} element={<PrivateRoute element={Account} />} />
- 
-            <Route path={"/booking"} element={<PublicRoute element={BookingPage} />}/>
 
-            {/* DEFAULT */}
+            <Route path={"/booking"} element={<PublicRoute element={BookingPage} />} />
+            <Route path={"/tickets/*"} element={<PublicRoute element={TicketsRoutes} />} />
+
             <Route path='*' element={<Navigate to="/flight-search" />} />
         </Routes>
     );
---
+++ src/app/tickets/search-tickets.page.tsx
@@ -0,0 +1,14 @@
+import Layout from "components/layout.comp";
+import { FC } from "react";
+
+const SearchTicketsPage: FC = () => {
+  return (
+    <>
+      <Layout>
+        <div>Layout</div>
+      </Layout>
+    </>
+  );
+}
+
+export default SearchTicketsPage;
