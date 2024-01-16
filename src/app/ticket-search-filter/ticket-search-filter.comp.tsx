8b637054bc59ac10e2f0b4c5774cdc3ffe66588b
tantality <angel.tretyak@mail.ru>
2024-01-16 13:20:55 +0300

    feat: add a passenger-amount-counter comp

--- src/app/ticket-search-filter/ticket-search-filter.comp.tsx
+++ src/app/ticket-search-filter/ticket-search-filter.comp.tsx
@@ -2,10 +2,12 @@ import { Button, SelectChangeEvent, Stack } from "@mui/material";
 import { Cities } from "enums/cities.enum";
 import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
 import { FC } from "react";
-import Select from "./components/select/select.comp";
-import { specifyArrivalTime, specifyDepartureTime, specifyDestinationCity, specifyOriginCity } from "./store/ticket-search-filter.actions";
+import Select from "./components/select.comp";
+import { specifyArrivalTime, specifyDepartureTime, specifyDestinationCity, specifyOriginCity, specifyPassengerAmount } from "./store/ticket-search-filter.actions";
 import { tickerSearchFilterSelector } from "./store/ticket-search-filter.selectors";
 import { DatePicker } from "@mui/x-date-pickers";
+import PassengerAmountCounter from "./components/passenger-amount-counter.comp";
+import { PassengerAmount } from "./constants/passenger-amount.constants";
 
 interface TickerSearchFilterProps {
   onSearchButtonClick: () => void;
@@ -32,7 +34,21 @@ const TickerSearchFilter: FC<TickerSearchFilterProps> = ({ onSearchButtonClick, 
     dispatch(specifyArrivalTime(date));
   }
 
-  const { originCity, destinationCity, departureTime, arrivalTime } = useAppSelector(tickerSearchFilterSelector);
+  const handleReducePassengerAmount = () => {
+    const changedAmount = passengerAmount - 1;
+    if (changedAmount >= PassengerAmount.Min) {
+      dispatch(specifyPassengerAmount(changedAmount))
+    }
+  }
+
+  const handleIncreasePassengerAmount = () => {
+    const changedAmount = passengerAmount + 1;
+    if (changedAmount <= PassengerAmount.Max) {
+      dispatch(specifyPassengerAmount(changedAmount))
+    }
+  }
+
+  const { originCity, destinationCity, departureTime, arrivalTime, passengerAmount } = useAppSelector(tickerSearchFilterSelector);
   const cities = Object.values(Cities);
 
   const departureDate = departureTime ? new Date(departureTime) : null;
@@ -46,10 +62,11 @@ const TickerSearchFilter: FC<TickerSearchFilterProps> = ({ onSearchButtonClick, 
           <Select labelId="destination-city-label" id="destination-city-select" label="Destination city" onSelectChange={handleDestinationCityChange} selectValues={cities} currentValue={destinationCity} />
           <DatePicker sx={{ minWidth: 170, flexGrow: 1 }} label="Departure date" value={departureDate} disablePast={true} onChange={handleDepartureDateChange} />
           <DatePicker sx={{ minWidth: 170, flexGrow: 1 }} label="Arrival date" value={arrivalDate} disablePast={true} onChange={handleArrivalDateChange} />
-          <Button variant="contained" sx={{ flexGrow: 1, borderRadius: 0, minWidth: 200 }} onClick={onSearchButtonClick}>Search</Button>
+          <PassengerAmountCounter passengerAmount={passengerAmount} onReducePassengerAmountClick={handleReducePassengerAmount} onIncreasePassengerAmountClick={handleIncreasePassengerAmount} />
+          <Button variant="contained" sx={{ flexGrow: 1, borderRadius: 0, minWidth: 200, fontSize: '1.1rem' }} onClick={onSearchButtonClick}>Search</Button>
         </Stack >
       </form>
-    </div>
+    </div >
   );
 }
 
