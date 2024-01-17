c364d57baae024cca7d1a8e325fe0b8eae1a09c7
tantality <angel.tretyak@mail.ru>
2024-01-17 21:24:53 +0300

    feat: add a button click handler

--- src/app/main/main.page.tsx
+++ src/app/main/main.page.tsx
@@ -1,7 +1,11 @@
 import { Stack, styled } from '@mui/material';
 import { StackProps } from '@mui/system';
+import { tickerSearchFilterSelector } from 'app/ticket-search-filter/store/ticket-search-filter.selectors';
 import TickerSearchFilter from 'app/ticket-search-filter/ticket-search-filter.comp'
+import { getAllTickets } from 'app/tickets/store/tickets.actions';
 import Layout from 'components/layout.comp';
+import { useAppSelector, useAppDispatch } from 'hooks/redux.hooks';
+import { useNavigate } from 'react-router-dom';
 
 const StyledMain = styled('div')((props) => ({
   height: '99vh',
@@ -14,11 +18,25 @@ const StyledFlex = styled(Stack)<StackProps>((props) => ({
 }));
 
 export const Main = () => {
+  const navigate = useNavigate();
+  const { originCity, destinationCity, departureTime, passengerAmount } = useAppSelector(tickerSearchFilterSelector);
+  const dispatch = useAppDispatch();
+
+  const handleSearchButtonClick = () => {
+    dispatch(getAllTickets({
+      query: {
+        originCity, destinationCity, departureTime: new Date(departureTime), passengerAmount, pageNumber: 1, pageSize: 20
+      }
+    }))
+
+    navigate('/tickets/search');
+  }
+
   return (
     <Layout>
       <StyledMain>
         <StyledFlex>
-          <TickerSearchFilter onSearchButtonClick={() => console.log('ef')} />
+          <TickerSearchFilter onSearchButtonClick={handleSearchButtonClick} />
         </StyledFlex>
       </StyledMain>
     </Layout>
