822436e3227205ed779c55290a502a45ce5837e2
tantality <angel.tretyak@mail.ru>
2024-01-17 21:22:48 +0300

    feat: add styles to a component

--- src/app/main/main.page.tsx
+++ src/app/main/main.page.tsx
@@ -1,10 +1,26 @@
+import { Stack, styled } from '@mui/material';
+import { StackProps } from '@mui/system';
 import TickerSearchFilter from 'app/ticket-search-filter/ticket-search-filter.comp'
-import React from 'react'
+import Layout from 'components/layout.comp';
+
+const StyledMain = styled('div')((props) => ({
+  height: '99vh',
+}));
+
+const StyledFlex = styled(Stack)<StackProps>((props) => ({
+  display: 'flex',
+  height: '100%',
+  justifyContent: 'center'
+}));
 
 export const Main = () => {
   return (
-    <>
-      <TickerSearchFilter onSearchButtonClick={() => console.log('ef')} />
-    </>
+    <Layout>
+      <StyledMain>
+        <StyledFlex>
+          <TickerSearchFilter onSearchButtonClick={() => console.log('ef')} />
+        </StyledFlex>
+      </StyledMain>
+    </Layout>
   )
 }
