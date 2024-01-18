eb85009e76b59feb1ec96f721f2d058eac51cd39
tantality <angel.tretyak@mail.ru>
2024-01-18 12:57:59 +0300

    feat: add pagination and points to show varios data

--- src/app/tickets/components/ticket-list.comp.tsx
+++ src/app/tickets/components/ticket-list.comp.tsx
@@ -1,17 +1,65 @@
-import { Stack } from "@mui/material";
+import { Pagination, Stack } from "@mui/material";
+import { tickerSearchFilterSelector } from "app/ticket-search-filter/store/ticket-search-filter.selectors";
 import CenteredLoader from "components/centered-loader.comp";
-import { useAppSelector } from "hooks/redux.hooks";
-import { FC } from "react";
+import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
+import { ChangeEvent, FC, useEffect, useState } from "react";
+import { calculatePageCount } from "utils/calculate-page-count.utils";
+import { getAllTickets } from "../store/tickets.actions";
 import { ticketsSelector } from "../store/tickets.selectors";
 import TicketCards from "./ticket-cards.comp";
 
+const PAGE_SIZE = 20;
+
 const TicketList: FC = () => {
-  const { isPending, tickets } = useAppSelector(ticketsSelector);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isRenderBeforeFirstRequest, setIsRenderBeforeFirstRequest] = useState<boolean>(true);
+  const dispatch = useAppDispatch();
+  const { isPending, tickets, count, errors } = useAppSelector(ticketsSelector);
+  const { originCity, destinationCity, departureTime, arrivalTime, passengerAmount } = useAppSelector(tickerSearchFilterSelector);
+
+  console.log(originCity, destinationCity, departureTime, arrivalTime, passengerAmount)
+
+  const handleCurrentPageChange = (event: ChangeEvent<unknown>, page: number): void => {
+    setCurrentPage(page);
+    setIsRenderBeforeFirstRequest(false);
+  };
+
+  useEffect(() => {
+    if (!isRenderBeforeFirstRequest) {
+      dispatch(getAllTickets({
+        query: {
+          originCity, destinationCity, departureTime: new Date(departureTime), passengerAmount,
+          pageNumber: currentPage,
+          pageSize: PAGE_SIZE,
+        }
+      }))
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, currentPage])
+
+  if (isPending.tickets) {
+    return <CenteredLoader />
+  }
+
+  if (errors.tickets) {
+    return <div>Error</div>
+  }
+
+  if (count === null) {
+    return <div>Let's look for tickets</div>
+  }
+
+  if (count === 0) {
+    return <div>No tickets</div>
+  }
+
+  const pageCount = calculatePageCount(count, PAGE_SIZE);
 
   return (
     <section className="ticket-list">
       <Stack alignItems="center" spacing="50px">
-        {isPending.tickets ? <CenteredLoader /> : <TicketCards tickets={tickets} />}
+        <TicketCards tickets={tickets} />
+        {pageCount > 1 && <Pagination onChange={handleCurrentPageChange} page={currentPage} count={pageCount} size="large" />}
       </Stack>
     </section>
   );
