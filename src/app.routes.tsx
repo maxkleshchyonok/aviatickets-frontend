7b7e74ec512c5e7c18de1a862212a6f7adccc255
tantality <angel.tretyak@mail.ru>
2024-01-21 02:11:25 +0300

    feat: delete a main page

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,6 +1,5 @@
 import React, { FC, Suspense } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
-import { Main } from "./app/main/main.page";
 import AuthRoutes from "./app/auth/auth.routes";
 import { Account } from "./app/account/Account";
 import { BookingPage } from "./app/booking";
@@ -27,8 +26,6 @@ const TicketsRoutes = React.lazy(() => import('app/tickets'));
 export const AppRoutes = () => {
     return (
         <Routes>
-            {/* PUBLIC */}
-            <Route path={"/flight-search/*"} element={<PublicRoute element={Main} />} />
 
             {/* PUBLIC */}
             <Route path={"/auth/*"} element={<PublicRoute element={AuthRoutes} />} />
