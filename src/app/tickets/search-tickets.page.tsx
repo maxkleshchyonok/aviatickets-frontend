66cc235ddef1cea5aaaf2f04c7ad97bb8728666f
tantality <angel.tretyak@mail.ru>
2024-01-21 02:19:13 +0300

    feat: implement getting cities

--- src/app/tickets/search-tickets.page.tsx
+++ src/app/tickets/search-tickets.page.tsx
@@ -2,11 +2,17 @@ import { Stack, styled } from "@mui/material";
 import { StackProps } from "@mui/system";
 import { tickerSearchFilterSelector } from "app/ticket-search-filter/store/ticket-search-filter.selectors";
 import TickerSearchFilter from "app/ticket-search-filter/ticket-search-filter.comp";
+import CenteredLoader from "components/centered-loader.comp";
 import Layout from "components/layout.comp";
 import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import SearchTicketsErrorPage from "./components/search-tickets-page-error.comp";
 import TicketList from "./components/ticket-list.comp";
-import { getAllTickets } from "./store/tickets.actions";
+import { ticketSearchFilterSchema } from "app/ticket-search-filter/validation-schemas/ticket-search-filter.schema";
+import { useForm } from "react-hook-form";
+import { citiesSelector } from "app/cities/store/cities.selectors";
+import { getAllCities } from "app/cities/store/cities.actions";
 
 const PAGE_SIZE = 20;
 
@@ -17,27 +23,51 @@ const StyledStack = styled(Stack)<StackProps>((props) => ({
 const SearchTicketsPage: FC = () => {
   const dispatch = useAppDispatch();
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const { originCity, destinationCity, departureTime, passengerAmount } = useAppSelector(tickerSearchFilterSelector);
+
+  const { filter } = useAppSelector(tickerSearchFilterSelector);
+  const { cities, isPending, errors } = useAppSelector(citiesSelector);
+
+  const { control, formState: { errors: validationErrors }, trigger } = useForm({
+    mode: "all",
+    resolver: yupResolver(ticketSearchFilterSchema),
+    defaultValues: { ...filter, arrivalTime: undefined, departureTime: undefined, }
+  });
 
   const handleSearchButtonClick = () => {
-    const currentPage = 1;
-    dispatch(getAllTickets({
-      query: {
-        originCity,
-        destinationCity,
-        departureTime: new Date(departureTime),
-        passengerAmount,
-        pageNumber: currentPage,
-        pageSize: PAGE_SIZE,
-      }
-    }))
-    setCurrentPage(currentPage);
+    trigger("originCity")
+    trigger("destinationCity")
+  }
+
+  useEffect(() => {
+    if (cities === null) {
+      dispatch(getAllCities())
+    }
+  }, [dispatch, cities])
+
+  if (isPending.cities) {
+    return (
+      <Layout>
+        <CenteredLoader />
+      </Layout>
+    )
+  }
+
+  if (errors.cities) {
+    return (
+      <Layout>
+        <SearchTicketsErrorPage />
+      </Layout>
+    )
+  }
+
+  if (!cities) {
+    return null;
   }
 
   return (
     <Layout>
       <StyledStack>
-        <TickerSearchFilter onSearchButtonClick={handleSearchButtonClick} />
+        <TickerSearchFilter onSearchButtonClick={handleSearchButtonClick} cities={cities} control={control} validationErrors={validationErrors} />
         <TicketList pageSize={PAGE_SIZE} currentPage={currentPage} setCurrentPage={setCurrentPage} />
       </StyledStack>
     </Layout>
