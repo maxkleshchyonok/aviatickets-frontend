8b2979d39e2619334246616e5d522e17bb79e6d5
tantality <angel.tretyak@mail.ru>
2024-01-21 23:03:08 +0300

    refactor: change erros messages

--- src/app/ticket-search-filter/validation-schemas/ticket-search-filter.schema.tsx
+++ src/app/ticket-search-filter/validation-schemas/ticket-search-filter.schema.tsx
@@ -1,19 +1,17 @@
 import { object, string, ref, number, date, InferType } from "yup";
 import { PassengerAmount } from "../constants/passenger-amount.constants";
 
-
 export const ticketSearchFilterSchema = object({
   originCity: string().required(),
   destinationCity: string()
     .required()
-    .notOneOf([ref("originCity")], "Destination city must be different from originCity"),
+    .notOneOf([ref("originCity")], "destinationCity must be different from originCity"),
   departureTime: date().required(),
   arrivalTime: date().optional().min(
     ref('departureTime'),
-    "arrival date must be later than departure date"
+    "arrivalDate must be later than departureDate"
   ),
   passengerAmount: number().required().min(PassengerAmount.Min).max(PassengerAmount.Max)
 });
-
 
 export type TicketSearchFilterYup = InferType<typeof ticketSearchFilterSchema>;
