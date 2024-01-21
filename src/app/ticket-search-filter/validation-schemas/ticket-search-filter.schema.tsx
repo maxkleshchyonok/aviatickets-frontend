1fc5b8a3b9cb0bb8d34a5cfa446d21108ac44621
tantality <angel.tretyak@mail.ru>
2024-01-21 20:12:41 +0300

    feat: update a validation schema

--- src/app/ticket-search-filter/validation-schemas/ticket-search-filter.schema.tsx
+++ src/app/ticket-search-filter/validation-schemas/ticket-search-filter.schema.tsx
@@ -1,13 +1,17 @@
 import { object, string, ref, number, date, InferType } from "yup";
 import { PassengerAmount } from "../constants/passenger-amount.constants";
 
+
 export const ticketSearchFilterSchema = object({
   originCity: string().required(),
   destinationCity: string()
     .required()
     .notOneOf([ref("originCity")], "Destination city must be different from originCity"),
   departureTime: date().required(),
-  arrivalTime: date().optional(),
+  arrivalTime: date().optional().min(
+    ref('departureTime'),
+    "arrival date must be later than departure date"
+  ),
   passengerAmount: number().required().min(PassengerAmount.Min).max(PassengerAmount.Max)
 });
 
