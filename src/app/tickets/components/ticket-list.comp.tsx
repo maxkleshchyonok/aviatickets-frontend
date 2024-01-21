ba2fcfb1a1c986e07dc948270d740987a1fd7161
tantality <angel.tretyak@mail.ru>
2024-01-21 18:16:08 +0300

    feat: remove a dispatch from a component

--- src/app/tickets/components/ticket-list.comp.tsx
+++ src/app/tickets/components/ticket-list.comp.tsx
@@ -1,10 +1,8 @@
 import { Pagination, Stack, StackProps, styled } from "@mui/material";
-import { tickerSearchFilterSelector } from "app/ticket-search-filter/store/ticket-search-filter.selectors";
 import CenteredLoader from "components/centered-loader.comp";
-import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
-import { ChangeEvent, FC, useEffect, useState } from "react";
+import { useAppSelector } from "hooks/redux.hooks";
+import { ChangeEvent, Dispatch, FC, SetStateAction } from "react";
 import { calculatePageCount } from "utils/calculate-page-count.utils";
-import { getAllTickets } from "../store/tickets.actions";
 import { ticketsSelector } from "../store/tickets.selectors";
 import NoTickets from "./no-tickets.comp";
 import TicketCards from "./ticket-cards.comp";
@@ -20,38 +18,11 @@ const StyledStack = styled(Stack)<StackProps>((props) => ({
 interface TicketListProps {
   pageSize: number;
   currentPage: number;
-  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
 }
 
 const TicketList: FC<TicketListProps> = ({ pageSize, currentPage, setCurrentPage }) => {
-  const [isRenderBeforeFirstRequest, setIsRenderBeforeFirstRequest] = useState<boolean>(true);
-  const dispatch = useAppDispatch();
   const { isPending, tickets, count, errors } = useAppSelector(ticketsSelector);
-  const { originCity, destinationCity, departureTime, passengerAmount } = useAppSelector(tickerSearchFilterSelector);
-
-  const handleCurrentPageChange = (event: ChangeEvent<unknown>, page: number): void => {
-    setCurrentPage(page);
-    setIsRenderBeforeFirstRequest(false);
-  };
-
-  useEffect(() => {
-    if (isRenderBeforeFirstRequest) {
-      return;
-    }
-
-    dispatch(getAllTickets({
-      query: {
-        originCity,
-        destinationCity,
-        departureTime: new Date(departureTime),
-        passengerAmount,
-        pageNumber: currentPage,
-        pageSize,
-      }
-    }));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, currentPage])
 
   if (isPending.tickets) {
     return <CenteredLoader />;
@@ -70,6 +41,10 @@ const TicketList: FC<TicketListProps> = ({ pageSize, currentPage, setCurrentPage
   }
 
   const pageCount = calculatePageCount(count, pageSize);
+
+  const handleCurrentPageChange = (event: ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+  }
 
   return (
     <StyledTicketList>
