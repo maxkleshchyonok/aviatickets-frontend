abc0c38970ad3ee15659d7ba09b970828c338f1a
ilik-sh <i.shyshparonak@gmail.com>
2024-01-23 00:02:18 +0300

    feat: add lazy imports to main router

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,51 +1,46 @@
 import React, { FC, Suspense } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
 import { Main } from "./app/main/Main";
-import AuthRoutes from "./app/auth/auth.routes";
 import { Account } from "./app/account/Account";
 import { BookingPage } from "./app/booking";
+import { LocalStorageKeys } from "enums/local-storage-keys.enum";
 
-
-// ======= private route ======= //
 const PrivateRoute: FC<{ element: any }> = ({ element: Element }) => {
-    return sessionStorage.getItem('access_token') ? (
-        <Suspense fallback={<div />}>
-            <div><Element /></div>
-        </Suspense>
-    ) : (
-        <Navigate to={"/auth/login"} />
-    );
+  return sessionStorage.getItem(LocalStorageKeys.AccessToken) ? (
+    <Suspense fallback={<div />}>
+      <div>
+        <Element />
+      </div>
+    </Suspense>
+  ) : (
+    <Navigate to={"/auth/login"} />
+  );
 };
 
-// ======= public route ======= //
 const PublicRoute: FC<{ element: any }> = ({ element: Element }) => (
-    <Suspense fallback={<div />}>
-        <Element />
-    </Suspense>
+  <Suspense fallback={<div />}>
+    <Element />
+  </Suspense>
 );
 
-// ======= pages ======= //
-// const MainPage = React.lazy(() => import("./app/main/Main"));
-// const BookingPage = React.lazy(() => import('./app/booking'));
+const AuthRoutes = React.lazy(() => import("app/auth"));
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            {/* PUBLIC */}
-            <Route path={"/flight-search/*"} element={<PublicRoute element={Main} />} />
-
-            {/* PUBLIC */}
-            <Route path={"/auth/*"} element={<PublicRoute element={AuthRoutes} />} />
-
-            {/* PRIVATE */}
-            <Route path={"/account"} element={<PrivateRoute element={Account} />} />
- 
-            <Route path={"/booking"} element={<PublicRoute element={BookingPage} />}/>
-
-            {/* DEFAULT */}
-            <Route path='*' element={<Navigate to="/flight-search" />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route
+        path={"/flight-search/*"}
+        element={<PublicRoute element={Main} />}
+      />
+      <Route path={"/auth/*"} element={<PublicRoute element={AuthRoutes} />} />
+      <Route path={"/account"} element={<PrivateRoute element={Account} />} />
+      <Route
+        path={"/booking"}
+        element={<PublicRoute element={BookingPage} />}
+      />
+      <Route path="*" element={<Navigate to="/flight-search" />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
