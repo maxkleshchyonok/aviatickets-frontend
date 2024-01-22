aab0b161d3548aebbca6c39d0f90838219096661
ilik-sh <i.shyshparonak@gmail.com>
2024-01-23 00:12:20 +0300

    refactor: change action typing and names

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,16 +1,17 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import {
-  ChangePasswordDto,
-  CreateUserDto,
-  ForgotPasswordDto,
-  ForgotPasswordResponse,
-  RegisterUserDto,
-  ResetPasswordDto,
-  VerifyCodeDto,
-} from "app/auth/types/types";
+import { ChangePasswordForm } from "../types/forms/change-password.form";
+import { ForgotPasswordForm } from "../types/forms/forgot-password.form";
+import { ResetPasswordForm } from "../types/forms/reset-password.form";
+import { SignInForm } from "../types/forms/sign-in.form";
+import { SignUpForm } from "../types/forms/sign-up.form";
+import { VerifyCodeForm } from "../types/forms/verify-code.form";
 import { ApiError } from "aviatickets-submodule/libs/types/api.error";
 import axios from "axios";
 import repository from "repository";
+import { AuthDto } from "../types/auth.dto";
+import { ForgotPasswordDto } from "../types/forgot-password.dto";
+import { LocalStorageKeys } from "enums/local-storage-keys.enum";
+import { VerifyDto } from "../types/verify.dto";
 
 type User = {
   id: string;
@@ -20,14 +21,14 @@ type User = {
   refreshToken: string;
 };
 
-export const loginUser = createAsyncThunk<
-  User,
-  CreateUserDto,
+export const signIn = createAsyncThunk<
+  AuthDto,
+  SignInForm,
   { rejectValue: ApiError | undefined }
->("loginUser", async (data, { rejectWithValue }) => {
+>("sgnIn", async (data, { rejectWithValue }) => {
   try {
     const response = await repository.post("/auth/signin", data);
-    sessionStorage.setItem("access_token", response.data.accessToken);
+    localStorage.setItem("access_token", response.data.accessToken);
     sessionStorage.setItem("userId", response.data.id);
     localStorage.setItem("refresh_token", response.data.refreshToken);
     return response.data;
@@ -42,31 +43,13 @@ export const loginUser = createAsyncThunk<
   }
 });
 
-export const registerUser = createAsyncThunk<User, RegisterUserDto>(
-  "registerUser",
-  async (data, { rejectWithValue }) => {
-    try {
-      const response = await repository.post("/auth/signup", data);
-      return response.data;
-    } catch (error) {
-      if (axios.isAxiosError<ApiError>(error)) {
-        return rejectWithValue(error.response?.data);
-      }
-      return rejectWithValue({
-        message: "Unknown erorr",
-        statusCode: 500,
-      } as ApiError);
-    }
-  }
-);
-
-export const forgotPassword = createAsyncThunk<
-  ForgotPasswordResponse,
-  ForgotPasswordDto
->("forgotPassword", async (data, { rejectWithValue }) => {
+export const signUp = createAsyncThunk<
+  AuthDto,
+  SignUpForm,
+  { rejectValue: ApiError | undefined }
+>("signUp", async (data, { rejectWithValue }) => {
   try {
-    const response = await repository.post("/auth/forgot-password", data);
-    localStorage.setItem("reset_token", response.data);
+    const response = await repository.post("/auth/signup", data);
     return response.data;
   } catch (error) {
     if (axios.isAxiosError<ApiError>(error)) {
@@ -79,31 +62,49 @@ export const forgotPassword = createAsyncThunk<
   }
 });
 
-export const resetPassword = createAsyncThunk<boolean, ResetPasswordDto>(
-  "resetPassword",
-  async (data, { rejectWithValue }) => {
-    try {
-      const response = await repository.post("/auth/reset-password", data);
-      if (response.status !== 200) {
-        alert("Operation failed");
-        return false;
-      }
-      return true;
-    } catch (error) {
-      if (axios.isAxiosError<ApiError>(error)) {
-        return rejectWithValue(error.response?.data);
-      }
-      return rejectWithValue({
-        message: "Unknown erorr",
-        statusCode: 500,
-      } as ApiError);
+export const forgotPassword = createAsyncThunk<
+  ForgotPasswordDto,
+  ForgotPasswordForm,
+  { rejectValue: ApiError | undefined }
+>("forgotPassword", async (data, { rejectWithValue }) => {
+  try {
+    const response = await repository.post("/auth/forgot-password", data);
+    localStorage.setItem(LocalStorageKeys.ResetToken, response.data);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError<ApiError>(error)) {
+      return rejectWithValue(error.response?.data);
     }
+    return rejectWithValue({
+      message: "Unknown erorr",
+      statusCode: 500,
+    } as ApiError);
   }
-);
+});
 
-export const verifyResetCode = createAsyncThunk<
-  string | boolean,
-  VerifyCodeDto
+export const resetPassword = createAsyncThunk<
+  boolean,
+  ResetPasswordForm,
+  { rejectValue: ApiError | undefined }
+>("resetPassword", async (data, { rejectWithValue }) => {
+  try {
+    await repository.post("/auth/reset-password", data);
+    return true;
+  } catch (error) {
+    if (axios.isAxiosError<ApiError>(error)) {
+      return rejectWithValue(error.response?.data);
+    }
+    return rejectWithValue({
+      message: "Unknown erorr",
+      statusCode: 500,
+    } as ApiError);
+  }
+});
+
+export const verifyCode = createAsyncThunk<
+  VerifyDto,
+  VerifyCodeForm,
+  { rejectValue: ApiError | undefined }
 >("verifyCode", async (data, { rejectWithValue }) => {
   try {
     const response = await repository.post("/auth/verify", data);
@@ -125,36 +126,38 @@ export const verifyResetCode = createAsyncThunk<
   }
 });
 
-export const changePassword = createAsyncThunk<void, ChangePasswordDto>(
-  "changePassword",
-  async (data, { rejectWithValue }) => {
-    try {
-      await repository.post("/auth/change-password", data);
-    } catch (error) {
-      if (axios.isAxiosError<ApiError>(error)) {
-        return rejectWithValue(error.response?.data);
-      }
-      return rejectWithValue({
-        message: "Unknown erorr",
-        statusCode: 500,
-      } as ApiError);
+export const changePassword = createAsyncThunk<
+  void,
+  ChangePasswordForm,
+  { rejectValue: ApiError | undefined }
+>("changePassword", async (data, { rejectWithValue }) => {
+  try {
+    await repository.post("/auth/change-password", data);
+  } catch (error) {
+    if (axios.isAxiosError<ApiError>(error)) {
+      return rejectWithValue(error.response?.data);
     }
+    return rejectWithValue({
+      message: "Unknown erorr",
+      statusCode: 500,
+    } as ApiError);
   }
-);
+});
 
-export const logout = createAsyncThunk(
-  "logout",
-  async (_, { rejectWithValue }) => {
-    try {
-      await repository.post("/auth/signout");
-    } catch (error) {
-      if (axios.isAxiosError<ApiError>(error)) {
-        return rejectWithValue(error.response?.data);
-      }
-      return rejectWithValue({
-        message: "Unknown erorr",
-        statusCode: 500,
-      } as ApiError);
+export const signOut = createAsyncThunk<
+  void,
+  void,
+  { rejectValue: ApiError | undefined }
+>("logout", async (_, { rejectWithValue }) => {
+  try {
+    await repository.post("/auth/signout");
+  } catch (error) {
+    if (axios.isAxiosError<ApiError>(error)) {
+      return rejectWithValue(error.response?.data);
     }
+    return rejectWithValue({
+      message: "Unknown erorr",
+      statusCode: 500,
+    } as ApiError);
   }
-);
+});
