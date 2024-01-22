be81ba8f7094b9ecd472de875bf2e33b836dfc8c
ilik-sh <i.shyshparonak@gmail.com>
2024-01-22 23:57:46 +0300

    feat: create socket client instance

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from 'App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "App";
+import "./index.css";
+import { SocketClient } from "aviatickets-submodule/libs/socket/socket.client";
+import { Provider } from "react-redux";
+import store from "store";
 
-const container = document.getElementById('root') as HTMLElement;
+export const socketClient = new SocketClient();
+
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
