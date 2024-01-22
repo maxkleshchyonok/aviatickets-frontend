4732af372c0ba5ff586ad3062a35398633291e53
ilik-sh <i.shyshparonak@gmail.com>
2024-01-22 14:53:17 +0300

    refactor: split sign up page into components

--- src/app/auth/sign-up.page.tsx
+++ src/app/auth/sign-up.page.tsx
@@ -1,57 +1,47 @@
-import React, { useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { RegisterUserDto } from 'app/auth/types/types';
-import { registerUser } from 'app/auth/store/auth.actions';
-import { signUpValidationSchema } from 'app/auth/validation-schemas/functions';
-import { v4 as uuidv4 } from 'uuid';
-import { yupResolver } from '@hookform/resolvers/yup';
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useAppDispatch } from "hooks/redux.hooks";
+import { useNavigate } from "react-router-dom";
+import { RegisterUserDto } from "app/auth/types/types";
+import { registerUser } from "app/auth/store/auth.actions";
+import { signUpValidationSchema } from "app/auth/validation-schemas/functions";
+import { v4 as uuidv4 } from "uuid";
+import { yupResolver } from "@hookform/resolvers/yup";
+import SignUpForm from "./components/forms/sign-up-form.comp";
+import { ApiError } from "aviatickets-submodule/libs/types/api.error";
+import { enqueueSnackbar } from "notistack";
 
 const defaultTheme = createTheme();
 
 export function SignUpPage() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const dispatch = useAppDispatch();
 
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<RegisterUserDto>({
     resolver: yupResolver(signUpValidationSchema),
-    mode: 'onBlur'
+    mode: "onBlur",
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
   });
 
-  const handleTogglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleToggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
   const onSubmit: SubmitHandler<RegisterUserDto> = async (values) => {
-    let device = localStorage.getItem('device_id');
+    let device = localStorage.getItem("device_id");
 
     if (!device) {
       device = uuidv4();
-      localStorage.setItem('device_id', device);
+      localStorage.setItem("device_id", device);
     }
 
     const registerData: RegisterUserDto = {
@@ -62,9 +52,19 @@ export function SignUpPage() {
       confirmPassword: values.confirmPassword,
     };
 
-    const response = await dispatch<any>(registerUser(registerData));
-    if (response.meta.requestStatus === 'fulfilled') {
-      navigate('/flight-search');
+    const response = await dispatch(registerUser(registerData));
+    if (response.meta.requestStatus == "rejected") {
+      const payload = response.payload as ApiError;
+      enqueueSnackbar(payload.message, { variant: "error" });
+    }
+    if (response.meta.requestStatus == "fulfilled") {
+      setTimeout(() => {
+        navigate("/flight-search");
+      }, 1500);
+      enqueueSnackbar("Succesfully signed up", {
+        variant: "success",
+        autoHideDuration: 1500,
+      });
     }
   };
 
@@ -72,116 +72,11 @@ export function SignUpPage() {
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <TextField
-              {...register('firstName')}
-              margin="normal"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              name="firstName"
-              autoComplete="firstName"
-              autoFocus
-              error={!!errors.firstName}
-              helperText={errors.firstName?.message}
-            />
-            <TextField
-              {...register('lastName')}
-              margin="normal"
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="lastName"
-              autoFocus
-              error={!!errors.lastName}
-              helperText={errors.lastName?.message}
-            />
-            <TextField
-              {...register('email')}
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              error={!!errors.email}
-              helperText={errors.email?.message}
-            />
-            <TextField
-              {...register('password')}
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              autoComplete="current-password"
-              error={!!errors.password}
-              helperText={errors.password?.message}
-              InputProps={{
-                endAdornment: (
-                  <div onClick={handleTogglePasswordVisibility} style={{ cursor: 'pointer' }}>
-                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                  </div>
-                ),
-              }}
-            />
-            <TextField
-              {...register('confirmPassword')}
-              margin="normal"
-              required
-              fullWidth
-              name="confirmPassword"
-              label="Confirm Password"
-              type={showConfirmPassword ? 'text' : 'password'}
-              id="confirmPassword"
-              autoComplete="current-password"
-              error={!!errors.confirmPassword}
-              helperText={errors.confirmPassword?.message}
-              InputProps={{
-                endAdornment: (
-                  <div onClick={handleToggleConfirmPasswordVisibility} style={{ cursor: 'pointer' }}>
-                    {showConfirmPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                  </div>
-                ),
-              }}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
+        <SignUpForm
+          control={control}
+          onSubmit={handleSubmit(onSubmit)}
+          validationErrors={errors}
+        />
       </Container>
     </ThemeProvider>
   );
