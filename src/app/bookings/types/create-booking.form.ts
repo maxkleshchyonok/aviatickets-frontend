03ed90c7974d39a577b110c6d9e26a1dae7ec13b
tantality <angel.tretyak@mail.ru>
2024-01-23 02:38:56 +0300

    feat: update a dto

--- src/app/bookings/types/create-booking.form.ts
+++ src/app/bookings/types/create-booking.form.ts
@@ -1,10 +1,14 @@
-import { PassengerDto } from "aviatickets-submodule/libs/types/passenger.dto";
-
 export interface CreateBookingForm {
   price: number;
   originCity: string;
   destinationCity: string;
   toDestinationRoute: string[];
   toOriginRoute: string[];
-  passengers: PassengerDto[];
+  passengers: Passenger[];
+}
+
+interface Passenger {
+  firstName: string;
+  lastName: string;
+  passportId: string;
 }
