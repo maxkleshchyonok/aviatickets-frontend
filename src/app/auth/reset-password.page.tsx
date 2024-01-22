62e38ec78ccc9491eac9edcdec6bd64eb4ef4595
ilik-sh <i.shyshparonak@gmail.com>
2024-01-22 14:52:20 +0300

    refactor: split reset password page into components

--- src/app/auth/reset-password.page.tsx
+++ src/app/auth/reset-password.page.tsx
@@ -1,108 +1,65 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { ResetPasswordDto } from 'app/auth/types/types';
-import { resetPassword } from 'app/auth/store/auth.actions';
-import { resetValidationSchema } from 'app/auth/validation-schemas/functions';
-import { yupResolver } from '@hookform/resolvers/yup';
+import React from "react";
+import { useForm } from "react-hook-form";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useAppDispatch } from "hooks/redux.hooks";
+import { useNavigate } from "react-router-dom";
+import { ResetPasswordDto } from "app/auth/types/types";
+import { resetPassword } from "app/auth/store/auth.actions";
+import { resetValidationSchema } from "app/auth/validation-schemas/functions";
+import { yupResolver } from "@hookform/resolvers/yup";
+import ResetPasswordForm from "./components/forms/reset-password-form.comp";
+import { ApiError } from "aviatickets-submodule/libs/types/api.error";
+import { enqueueSnackbar } from "notistack";
 
 const defaultTheme = createTheme();
 
 export function ResetPasswordPage() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { handleSubmit, register, formState: { errors } } = useForm<ResetPasswordDto>({
+  const dispatch = useAppDispatch();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ResetPasswordDto>({
     resolver: yupResolver(resetValidationSchema),
-    mode: 'onBlur'
+    mode: "onBlur",
+    defaultValues: { password: "", confirmPassword: "" },
   });
 
   const onSubmit = async (data: ResetPasswordDto) => {
     const resetData: ResetPasswordDto = {
       password: data.password,
-      confirmPassword: data.confirmPassword
+      confirmPassword: data.confirmPassword,
     };
 
-    const response = await dispatch<any>(resetPassword(resetData));
+    const response = await dispatch(resetPassword(resetData));
 
-    if (response.meta.requestStatus === 'fulfilled') {
-      navigate('/auth/login');
+    if (response.meta.requestStatus == "rejected") {
+      const payload = response.payload as ApiError;
+      enqueueSnackbar(payload.message, { variant: "error" });
     }
-
+    if (response.meta.requestStatus == "fulfilled") {
+      setTimeout(() => {
+        navigate("/auth/login");
+      }, 1500);
+      enqueueSnackbar("Password succesfully changed", {
+        variant: "success",
+        autoHideDuration: 1500,
+      });
+    }
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <AssignmentIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Reset Password
-          </Typography>
-          <form noValidate onSubmit={handleSubmit(onSubmit)}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="New Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              {...register('password')}
-              error={!!errors.password}
-              helperText={errors.password?.message}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              label="Confirm Password"
-              type="password"
-              id="confirmPassword"
-              autoComplete="confirm-password"
-              {...register('confirmPassword')}
-              error={!!errors.confirmPassword}
-              helperText={errors.confirmPassword?.message}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Change password
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="/auth/signin" variant="body2">
-                  {"Back to login"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
+        <ResetPasswordForm
+          control={control}
+          onSubmit={handleSubmit(onSubmit)}
+          validationErrors={errors}
+        />
       </Container>
     </ThemeProvider>
   );
