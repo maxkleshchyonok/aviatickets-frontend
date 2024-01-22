ad14710254cbf1ea22d220f3cf560fb5e2b3d3bc
ilik-sh <i.shyshparonak@gmail.com>
2024-01-22 23:55:31 +0300

    feat: create enum for auth paths

---
+++ src/enums/page-paths.enum.ts
@@ -0,0 +1,8 @@
+export enum AuthModulePagePaths {
+  SignIn = "/auth/signin",
+  SignUp = "/auth/signup",
+  ForgotPassword = "/auth/forgot",
+  ResetPassword = "/auth/reset",
+  ChangePassword = "/auth/change",
+  Verify = "/auth/verify",
+}
