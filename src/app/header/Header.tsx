65bc53fd95567f47123a85ea5aac8cb62774b15c
ilik-sh <i.shyshparonak@gmail.com>
2024-01-23 00:04:35 +0300

    refactor: change log out method naming

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import AirplaneTicketIcon from '@mui/icons-material/AirplaneTicket';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
-import { logout } from 'app/auth/store/auth.actions';
-import { useAppDispatch, useAppSelector } from 'hooks/redux.hooks';
-import { RootState } from 'store';
-import { authSelector } from 'app/auth/store/auth.selector';
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import AirplaneTicketIcon from "@mui/icons-material/AirplaneTicket";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { signOut } from "app/auth/store/auth.actions";
+import { useAppDispatch, useAppSelector } from "hooks/redux.hooks";
+import { RootState } from "store";
+import { authSelector } from "app/auth/store/auth.selector";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -40,7 +40,7 @@ export default function Header() {
     if (isAuth) {
       return setAnchorEl(event.currentTarget);
     }
-    navigate('/auth/login');
+    navigate("/auth/login");
   };
 
   const handleMobileMenuClose = () => {
@@ -58,25 +58,25 @@ export default function Header() {
 
   const handleLogout = async () => {
     handleMenuClose();
-    const response = await dispatch(logout());
-    if (response.meta.requestStatus == 'fulfilled') {
-      navigate('/auth/login');
+    const response = await dispatch(signOut());
+    if (response.meta.requestStatus == "fulfilled") {
+      navigate("/auth/login");
     }
-  }
+  };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
@@ -87,19 +87,19 @@ export default function Header() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
@@ -156,13 +156,17 @@ export default function Header() {
             variant="h6"
             noWrap
             component="div"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
+            sx={{ display: { xs: "none", sm: "block" } }}
           >
             Avia Tickets
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
               <Badge badgeContent={4} color="error">
                 <AirplaneTicketIcon />
               </Badge>
@@ -179,7 +183,7 @@ export default function Header() {
               <AccountCircle />
             </IconButton>
           </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="show more"
