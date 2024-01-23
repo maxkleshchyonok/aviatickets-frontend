5c6fa8cf2625657b8ce2aa040435698616b53880
tantality <angel.tretyak@mail.ru>
2024-01-23 15:10:45 +0300

    feat: display a ticket price and the number of passengers

--- src/app/bookings/components/ticket-price-details.comp.tsx
+++ src/app/bookings/components/ticket-price-details.comp.tsx
@@ -1,29 +1,37 @@
-import { Box, Divider, Typography } from "@mui/material"
+import { Box, Divider, Stack, StackProps, styled, Typography, TypographyProps } from "@mui/material"
+import { tickerSearchFilterSelector } from "app/ticket-search-filter/store/ticket-search-filter.selectors";
 import { TicketDto } from "app/tickets/types/ticket.dto";
+import { useAppSelector } from "hooks/redux.hooks";
 import { FC } from "react";
+import TicketPriceDetailsListItem from "./ticket-price-details-list-item.comp";
+import TicketPriceDetailsList from "./ticket-price-details-list.comp";
 
 interface TicketPriceDetailsProps {
   ticket: TicketDto;
 }
 
+const StyledContainer = styled(Stack)<StackProps>(() => ({
+  gridArea: 'ticketSummary',
+  flexDirection: 'column'
+}));
+
+const StyledTitle = styled(Typography)<TypographyProps>(() => ({
+  padding: '10px',
+  borderRadius: '20px 20px 0 0',
+  fontWeight: 600
+}));
+
 const TicketPriceDetails: FC<TicketPriceDetailsProps> = ({ ticket }) => {
-  //get passengers amount from the store to get total price
+  const { filter } = useAppSelector(tickerSearchFilterSelector);
+
   return (
-    <Box style={{ gridArea: 'ticketSummary', }}>
-      <Typography variant='h6' sx={{
-        // backgroundColor: 'lightGrey',
-        padding: '10px',
-        borderRadius: '20px 20px 0 0'
-      }}>Fare Summary</Typography>
+    <StyledContainer >
+      <StyledTitle variant='h6'>Ticket price details</StyledTitle>
       <Divider />
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: '10px'
-      }}>
-        <Typography variant='body1'>Ticket</Typography>
-        <Typography variant='body1'>Price</Typography>
-      </Box>
+      <TicketPriceDetailsList>
+        <TicketPriceDetailsListItem label="Ticket price" value={`${ticket.price}$`} />
+        <TicketPriceDetailsListItem label="Passenger amount" value={filter.passengerAmount} />
+      </TicketPriceDetailsList>
       <Box sx={{
         backgroundColor: 'lightGrey',
         padding: '10px',
@@ -34,7 +42,7 @@ const TicketPriceDetails: FC<TicketPriceDetailsProps> = ({ ticket }) => {
         <Typography variant='h6'>Total</Typography>
         <Typography variant='h6'>Total Price</Typography>
       </Box>
-    </Box>
+    </StyledContainer>
   )
 };
 
